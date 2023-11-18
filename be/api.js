bafb98fd496515f11da4ff4537eff9f367c5ee91
István Kádár <Istvan.Kadar@partner.bmwgroup.com>
2023-11-18 21:36:41 +0100

    cors disabled in be

--- be/api.js
+++ be/api.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const ai = require('./main');
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(express.json());
 
 /**
